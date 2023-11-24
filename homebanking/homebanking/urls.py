5d8fe3be99a53b9b26ffe9faeed053a000ba9323
Sebastian Zelarayan <142546669+sebaszelarayan@users.noreply.github.com>
2023-11-24 15:58:50 -0300

    sin errores...

--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path('prestamos/',Prestamos,name = 'Prestamos'),
     path('movimientos/',Movimiento,name = 'Movimientos'),
     path('tarjetas/',Tarjetas, name = 'tarjetas'),
-    path('admin/', admin.site.urls),
     path('register/', RegisterView.as_view(),name='register'),
     path('login/', LoginView.as_view(),name='login'),
     path('logout/', LogoutView.as_view(),name='logout'),
